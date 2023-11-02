bc0dab89c84a2bcf60aacc3c04b85cfaede46816
Abdul Wahab <abdul.wahab@codistan.org>
2023-11-02 22:10:20 +0500

    like tweet

--- src/app/api/likeTweet/route.ts
+++ src/app/api/likeTweet/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse } from "next/server";
 
 export async function POST (request:Request){
+
+    // implement function
     return NextResponse.json({
         message:"Yooo"
     })
